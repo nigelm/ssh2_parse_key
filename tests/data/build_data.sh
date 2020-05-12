1f2ff3265f9a2eac1c0799267fca103b052802b3
Nigel Metheringham <nigelm@cpan.org>
2020-05-12 15:15:38 +0100

    Update test data to use old openssh priv key format

--- tests/data/build_data.sh
+++ tests/data/build_data.sh
@@ -8,7 +8,8 @@
 # This must be run from within the data directory.
 #
 for keytype in dsa ecdsa ed25519 rsa; do
-    ssh-keygen -f test_key_${keytype} -N '' -t ${keytype} -C "Test ssh key in ${keytype} format"
+    # generating in old PEM format (other than ed25519 which has no old format)
+    ssh-keygen -f test_key_${keytype} -N '' -m PEM -t ${keytype} -C "Test ssh key in ${keytype} format"
     ##  ssh-keygen -e -f test_key_${keytype} -m RFC4716 >test_key_${keytype}_rfc4716
     ssh-keygen -e -f test_key_${keytype}.pub -m RFC4716 >test_key_${keytype}_rfc4716.pub
 done
