2a2e9c5805153b5e2775eaf075d743a698ff43b4
Nigel Metheringham <nigelm@cpan.org>
2020-09-26 15:23:41 +0100

    Fixed the module __init__.py
    No I do not know why I had the previous abomination - presume it was avoiding flake8 warnings.

--- ssh2_parse_key/__init__.py
+++ ssh2_parse_key/__init__.py
@@ -1,8 +1,10 @@
 """Top-level package for SSH2 Key Parsing."""
-from .ssh2_parse_key import Ssh2Key as _Ssh2Key
+from .ssh2_parse_key import Ssh2Key
 
-Ssh2Key = _Ssh2Key
+__all__ = ["Ssh2Key"]
 
 __author__ = "Nigel Metheringham"
 __email__ = "nigelm@cpan.org"
 __version__ = "0.2.3"
+
+# end
